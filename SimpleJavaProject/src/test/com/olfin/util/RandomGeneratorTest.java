511996b0fe6d6f90e69f2b2e9c59ded4dbba6e34
Jonathan Lowe <jonathan.x.lowe@gmail.com>
2013-04-28 22:31:52 +0100

    adding test

---
+++ SimpleJavaProject/src/test/com/olfin/util/RandomGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.olfin.util;
+
+import junit.framework.TestCase;
+
+public class RandomGeneratorTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testGo(){
+		
+		assert(1==1);
+	}
+
+}
