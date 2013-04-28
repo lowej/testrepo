95f2a8c7364c689ae48f810b8bdbe85b2520b1b7
Jonathan Lowe <jonathan.x.lowe@gmail.com>
2013-04-28 21:55:52 +0100

    added some stuff

--- SimpleJavaProject/src/com/olfin/util/RandomGenerator.java
+++ SimpleJavaProject/src/com/olfin/util/RandomGenerator.java
@@ -2,18 +2,34 @@ package com.olfin.util;
 
 import java.util.Date;
 import java.util.Random;
+import org.apache.log4j.*;
+import org.apache.log4j.chainsaw.Main;
 
 public class RandomGenerator {
 	
+	private static Logger log = Logger.getLogger(RandomGenerator.class);
+	
 	public static void main(String args[]){
+		
+		// Configure Log4J
+	    PropertyConfigurator.configure(Main.class.getClassLoader()
+	                                       .getResource("log4j.properties"));
+	    
+	    new RandomGenerator().go();
+		
+		
+	}
+	
+	void go(){
+		
 		
 		
 		long seed = new Date().getTime();
 		String randNumber = Integer.toString(new Random(seed).nextInt());
 		
-		System.out.println("Random string is: " + randNumber);
+		log.debug("Random string is: " + randNumber);
 		
-		System.out.println("This does the weather thing");
+		log.debug("This does the weather thing");
 	}
 
 }
